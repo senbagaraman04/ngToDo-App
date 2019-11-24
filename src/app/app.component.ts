e7e0e45bb4acdea43c9a82e6d36d1ce7c2c2b124
Senbagaraman Manoharan <senbagaraman04@gmail.com>
2019-11-24 19:07:44 +0530

    Inital Todo setup added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'toDoApp';
+  toDoContain = []; //Initialize an empty array 
+  returnMessage = null;
+  submitToDo(inputString){
+
+    console.log("Entered Input String");
+    console.log(inputString)      
+   this.toDoContain.push(inputString);
+   this.returnMessage = "Your Input has been received";  
+   
+  }
 }
