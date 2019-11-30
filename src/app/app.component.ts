c13b9e04dc028b46a10f4eda8b0806ee50b66b45
Senbagaraman Manoharan <senbagaraman04@gmail.com>
2019-11-30 18:37:33 +0530

    strike out issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'toDoApp';
+  toDoContain = []; //Initialize an empty array 
+  returnMessage = null;
+   submitToDo(inputString){
+    console.log("Entered Input String");
+    console.log(inputString)  
+    if (inputString ==  undefined || inputString == empty)
+    {
+      console.log("Empty");      
+    }  
+    else {
+      if(this.toDoContain.includes(inputString.toLowerCase())){
+        //this.returnMessage = " "; 
+    alert("Already Added");
+    inputString = empty; 
+    }
+    else 
+    {
+    this.toDoContain.push(inputString);
+    }
+    //this.returnMessage = "Your Input has been received"; 
+    }
+   
+  }
+
+  removeTask(removedItem){
+    console.log("Removed the data");
+    this.toDoContain.slice(this.toDoContain.indexOf(removedItem),1);
+  }
+
 }
